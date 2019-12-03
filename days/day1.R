ceb993df24158c3407b72497f0fb242aba7a278f
Will Bishop <wbishop@plenty.ag>
2019-12-02 16:35:46 -0800

    code simplification

--- days/day1.R
+++ days/day1.R
@@ -1,31 +1,25 @@
 # DAY 1 
 library(magrittr)
 library(purrr)
-
+module_masses <- readLines(
+  file.path("data-raw", "day1-part1.txt"), warn = FALSE) %>% 
+  as.numeric()
 
 # part 1 ====
 launch_fuel <- function(mass) {
   floor(mass / 3) - 2
 }
-module_masses <- readLines(
-  file.path("data-raw", "day1-part1.txt"), warn = FALSE) %>% 
-  as.numeric()
-total_fuel <- launch_fuel(module_masses) %>% sum()
-total_fuel
+fuel <- launch_fuel(module_masses) %>% sum()
+fuel
 
 
 # part 2 ====
 recursive_launch_fuel <- function(mass) {
   fuel <- launch_fuel(mass)
-  if (fuel > 0) {
-    extra_fuel <- recursive_launch_fuel(fuel)
-    return(fuel + extra_fuel)
-  } else {
-    return(0)
-  }
+  ifelse(fuel > 0, fuel + recursive_launch_fuel(fuel), 0)
 }
-total_recursive_fuel <- purrr::map_dbl(
+fuel <- purrr::map_dbl(
   module_masses, recursive_launch_fuel) %>% 
   sum()
-total_recursive_fuel
+fuel
 
