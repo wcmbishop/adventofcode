c5939d3420f88873ac795e14bf7213ff886cf9ef
Will Bishop <wbishop@plenty.ag>
2020-12-03 09:57:56 -0500

    change tibble to plural

--- 2020/day-02.R
+++ 2020/day-02.R
@@ -7,7 +7,7 @@ passwords_raw <- readLines(file.path("data-raw", "day-02", "passwords.txt"))
 
 
 # part 1 ====
-password <- tibble(raw_input = passwords_raw) %>% 
+passwords <- tibble(raw_input = passwords_raw) %>% 
   separate(raw_input, c("policy", "password"), sep = ": ", remove = FALSE) %>% 
   mutate(
     policy_min = as.integer(str_match(policy, "([\\d]+)-[\\d]+ .*")[, 2]),
@@ -16,18 +16,18 @@ password <- tibble(raw_input = passwords_raw) %>%
     char_count = str_count(password, policy_char),
     is_valid = char_count >= policy_min & char_count <= policy_max
   )
-password %>%
+passwords %>%
   count(is_valid)
 
 
 # part 2 ====
 # apply different password validation
-password <- password %>% 
+passwords <- passwords %>% 
   mutate(
     password_char_a = str_sub(password, policy_min, policy_min),
     password_char_b = str_sub(password, policy_max, policy_max),
     is_valid2 = xor(password_char_a == policy_char, 
                     password_char_b == policy_char)
   )
-password %>%
+passwords %>%
   count(is_valid2)
