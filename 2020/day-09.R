78baf9ca48c837f74004870f9929da5e82d456a6
Will Bishop <wbishop@plenty.ag>
2020-12-11 18:54:17 -0500

    switch while to for loop

--- 2020/day-09.R
+++ 2020/day-09.R
@@ -7,7 +7,7 @@ library(tibble)
 library(tidyr)
 
 # read input file
-all_numbers <- file.path("data-raw", "day-09", "numbers.txt") %>% 
+numbers <- file.path("data-raw", "day-09", "numbers.txt") %>% 
   readLines() %>% 
   as.numeric()
 preamble_length <- 25
@@ -17,32 +17,28 @@ has_addition_matches <- function(total, numbers) {
   total %in% combn(numbers, m = 2, sum)
 }
 # search for first number without matching sum
-index <- preamble_length + 1
-while (TRUE) {
-  if (index > length(all_numbers)) {
-    print("finished")
-    break
-  }
-  match <- has_addition_matches(
-    all_numbers[index], 
-    all_numbers[(index - preamble_length):(index - 1)]
+for (i in seq_along(numbers)) {
+  if (i <= preamble_length) next
+  has_match <- has_addition_matches(
+    total = numbers[i], numbers = numbers[(i - preamble_length):(i - 1)]
   )
-  if (match == FALSE) {
-    print(glue("found a non-motch: index {index}, number {number}",
-               index = index, number = all_numbers[index]))
+  if (has_match == FALSE) {
+    print(glue(
+      "found a non-motch: index {i}, number {number}",
+      i = i, number = numbers[i]
+    ))
     break
   }
-  index <- index + 1
 }
 
 
 # part 2 ====
 invalid_num <- 556543474
-indices <- seq_along(all_numbers)
+indices <- seq_along(numbers)
 for (chunk_start in indices) {
   chunk_found <- FALSE
   for (chunk_end in indices[indices > chunk_start]) {
-    chunk <- all_numbers[chunk_start:chunk_end]
+    chunk <- numbers[chunk_start:chunk_end]
     if (sum(chunk) == invalid_num) {
       print("chunk found!")
       print(chunk)
